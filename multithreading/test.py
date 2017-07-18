4f3f4a3d0806fe0955c051345847f6eacbf7ff4c
Gonzo <dggsax@gmail.com>
2017-07-18 14:41:40 -0400

    Messy but you get the idea

--- multithreading/test.py
+++ multithreading/test.py
@@ -9,70 +9,52 @@ import threading
 import time
 
 # Set up a logger
-logging.basicConfig(level=logging.DEBUG, # The level that we want it to Debug.
-                    format='[%(levelname)s] (%(threadName) - 10s) %(message)s')
-
-
-
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s] (%(threadName) - 10s) %(message)s'
+                    )
 
 def worker():
-    # This is really cool, general for all threads!
-    logging.debug('Starting')
-    time.sleep(2)
-    # Same here!
-    logging.debug('Exiting')
+    while True:
+        logging.debug("hi")
+    # try:
+    #     logging.debug("Hello!")
+    # except Exception:
+    #     logging.debug("death :(")
 
-def my_service():
-    logging.debug('Starting')
-    time.sleep(2)
-    logging.debug('Exiting')
+class threadManager(threading.Thread):
 
+    # Initialize
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, verbose=None):
+        threading.Thread.__init__(self, group=group, target=target, name=name,
+                                  verbose=verbose) 
+        self.name = name
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs
+        return
 
-def main():
-    t = threading.Thread(name='my_service', target=my_service)
-    w = threading.Thread(name='worker', target=worker)
-    w2 = threading.Thread(target=worker)
+    def run(self):
+        logging.debug('Starting with args: %s and kwargs: %s',self.args,self.kwargs)
+        return
+    def demon(self):
+        self.setDaemon(True)
+        return
+# Yeshua = threadManager( name='Yeshua' , daemon=True)
+# Yeshua = threadManager( name='Yeshua' )
+# Yeshua = threadManager( name='Yeshua' , target=worker())
+# Gonzo = threadManager( name='Gonzo' , target=worker())
+# Yeshua = threadManager( name='Yeshua' , target=worker() , daemon=True)
+# Yeshua.start()
+# Gonzo.start()
+# Yeshua.test()
+# print(Yeshua)
+# time.sleep(5)
+# print(Yeshua)
+# print(Gonzo)
 
+for i in range(5):
+    # t = threadManager(args=(i,))
+    t = threadManager(name="Joe"+str(i), target=worker)
+    # t.demon()
     t.start()
-    w.start()
-    w2.start()
-
-# class threadmanager(name, worker_function):
-#     def __init__(self,name,worker_function):
-#         # (Hopefully) make the thread
-#         self.name = self.threading.Thread(name=Str(name), worker=worker_function)
-        
-#         # Instantiate a loop to do shit
-#         self.loop()
-
-#     def loop(self):
-#         try:
-#             while True:
-#                 print(threading.currentThread().getName(), 'Yoooo')
-#                 time.sleep(3)
-#                 print(threading.currentThread().getName(), 'I"m back thot')
-#                 time.sleep(3)
-#         except:
-#             print("Damn :(")
-
-if __name__ == '__main__':
-    main()
-
-
-
-'''
-class SpectrumAnalyzer:
-
-    def __init__(self):
-        self.pa = pyaudio.PyAudio()
-        self.stream = self.pa.open(
-            format = self.FORMAT,
-            channels = self.CHANNELS, 
-            rate = self.RATE,
-            input = True,
-            output = False,
-            input_device_index = 0,
-            frames_per_buffer = self.CHUNK)
-    #     #Main Loop
-    #     self.loop()
-'''
